6a448bfa9a5fa14f863a7f016e364cf0f81ede92
Diego Laforet <dlaforet57@gmail.com>
2026-01-14 02:08:01 +0100

    1.0

---
+++ src/i18n/index.ts
@@ -0,0 +1,10 @@
+import es from "./json/es.json";
+import en from "./json/en.json";
+import de from "./json/de.json";
+import it from "./json/it.json";
+import ru from "./json/ru.json";
+import uk from "./json/uk.json";
+import pl from "./json/pl.json";
+import zh from "./json/zh.json";
+
+export const messages = { es, en, de, it, ru, uk, pl, zh };
